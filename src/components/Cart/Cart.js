7a343ceda488755abf8f73d9a6390bbba2aaf37a
konok101 <rockykonok@gmail.com>
2022-05-28 02:04:27 +0600

    condition use , || means mitta and && means true

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,8 +8,16 @@ const Cart = ({cart, handleRemoveCart}) => {
         <div>
             <h3>Items selected: {cart.length}</h3>
             {
-                cart.map(tShirt => <p>{tShirt.name } <button onClick={()=>handleRemoveCart(tShirt)}>X</button> </p>)
+                cart.map(tShirt =>
+                     <p>{tShirt.name } <button onClick={()=> handleRemoveCart(tShirt)}>X</button>
+                      </p>)
             }
+            {cart.length !==4 ? <p>keep adding</p>: <button>Remove  item</button>}
+            {cart.length === 3 && <div className='orange'>
+                <h3>Threee item added</h3>
+                <p>Now i provide 10% off</p>
+                 </div>}
+                 {cart.length === 0 || <p className='orange'>Yay you are buying</p>}
         </div>
     );
 };
